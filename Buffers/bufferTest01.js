8180a69e50f112df5e42cd735a3f4c2e6f8b6d6f
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-10-26 15:02:21 +0530

    Copy contents of Buffer 1 to buffer 2 and print

--- Buffers/bufferTest01.js
+++ Buffers/bufferTest01.js
@@ -6,3 +6,18 @@ for(var x = 0; x < buffer.length; x++){
 	content = buffer.slice(x,(x+1));	
 	console.log(" "+content);
 }
+
+
+var bufferOne = new Buffer("This is China");
+var bufferTwo = new Buffer(20),
+	startTarget = 0,
+	sourceStart = 8,
+	sourceEnd = 0;
+var bufferLength = bufferOne.length;
+sourceEnd = bufferLength;
+console.log("Buffer length "+bufferLength);
+bufferOne.copy(bufferTwo,startTarget,sourceStart,sourceEnd);
+
+console.log("Buffer1 "+bufferOne.toString());	
+console.log("Buffer2 "+bufferTwo.toString());	
+
